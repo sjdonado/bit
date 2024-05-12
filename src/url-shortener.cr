db42ed2b24a3cbf22338dd06751993ebe989781e
Juan Rodriguez <sjdonado@icloud.com>
2024-05-12 18:35:45 +0200

    feat: kemal setup

--- src/url-shortener.cr
+++ src/url-shortener.cr
@@ -1,6 +1,7 @@
-# TODO: Write documentation for `Url::Shortener`
-module Url::Shortener
-  VERSION = "0.1.0"
+require "kemal"
 
-  # TODO: Put your code here
+get "/" do
+  "Hello World!"
 end
+
+Kemal.run
