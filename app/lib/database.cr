a47722cd54647f674572809385464627be49b11a
Juan Rodriguez <sjdonado@icloud.com>
2024-05-13 22:54:49 +0200

    refactor: Link serializer - refer attr

--- app/lib/database.cr
+++ app/lib/database.cr
@@ -5,10 +5,14 @@ module App::Lib
   class Database
     extend Crecto::Repo
 
+    Query = Crecto::Repo::Query
+
     config do |conf|
       conf.uri = ENV["DATABASE_URL"]
     end
 
-    Crecto::DbLogger.set_handler(STDOUT)
+    if ENV["ENV"] == "development"
+      Crecto::DbLogger.set_handler(STDOUT)
+    end
   end
 end
--- app/serializers/link.cr
+++ app/serializers/link.cr
@@ -4,13 +4,16 @@ require "../models/link"
 
 module App::Serializers
   class Link
+    getter refer
+
     def initialize(@link : App::Models::Link)
+      @refer = "#{ENV["APP_URL"]}/#{@link.slug}"
     end
 
     def to_json(builder : JSON::Builder)
       builder.object do
         builder.field("id", @link.id)
-        builder.field("link", "#{ENV["APP_URL"]}/#{@link.slug}")
+        builder.field("refer", @refer)
         builder.field("origin", @link.url)
         builder.field("clicks", @link.click_counter)
       end
