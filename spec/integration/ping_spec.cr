814fd83d328946ffa6a9f9c451dc0503450e3fef
Juan Rodriguez <sjdonado@icloud.com>
2024-05-13 22:59:22 +0200

    test: ping spec

---
+++ spec/integration/ping_spec.cr
@@ -0,0 +1,10 @@
+require "../spec_helper"
+
+describe App::Controllers::Ping do
+  it "should return pong" do
+    get "/api/ping"
+
+    expected = {"pong" => "ok"}.to_json
+    response.body.should eq(expected)
+  end
+end
