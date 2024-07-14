1d207fae641f1262a542084601c596eb63c17145
Juan Rodriguez <sjdonado@icloud.com>
2024-07-14 09:26:25 +0200

    fix: log level error for production env

--- app/config/env.cr
+++ app/config/env.cr
@@ -1,10 +1,15 @@
+require "log"
+
 ENV["ENV"] ||= "development"
+ENV["APP_URL"] ||= "http://localhost:4000"
+ENV["DATABASE_URL"] ||= "sqlite3://./sqlite/data.db?journal_mode=wal&synchronous=normal&foreign_keys=true
+"
 
 {% if env("ENV") != "production" %}
   require "dotenv"
   Dotenv.load ".env.#{ENV["ENV"]}" # File must exist in non-production!
 {% end %}
 
-ENV["APP_URL"] ||= "http://localhost:4000"
-ENV["DATABASE_URL"] ||= "sqlite3://./sqlite/data.db?journal_mode=wal&synchronous=normal&foreign_keys=true
-"
+{% if env("ENV") == "production" %}
+  Log.setup(:error)
+{% end %}
