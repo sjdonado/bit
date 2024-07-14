a71f345f6608260b2d1b4b66c2ced0924da6e965
Juan Rodriguez <sjdonado@icloud.com>
2024-07-14 08:46:29 +0200

    refactor: APP_URL + DATABASE_URL default values

--- app/config/env.cr
+++ app/config/env.cr
@@ -4,3 +4,7 @@ ENV["ENV"] ||= "development"
   require "dotenv"
   Dotenv.load ".env.#{ENV["ENV"]}" # File must exist in non-production!
 {% end %}
+
+ENV["APP_URL"] ||= "http://localhost:4000"
+ENV["DATABASE_URL"] ||= "sqlite3://./sqlite/data.db?journal_mode=wal&synchronous=normal&foreign_keys=true
+"
