98dedc4494421d27b9c4734a26c80ae343e59195
Juan Rodriguez <sjdonado@icloud.com>
2024-10-27 11:07:59 +0100

    refactor: powered_by_header kemal config

--- app/config/kemal.cr
+++ app/config/kemal.cr
@@ -3,3 +3,5 @@ require "kemal"
 Kemal.config.env = ENV["ENV"]? || "development"
 Kemal.config.port = ENV["PORT"]?.try(&.to_i) || 4000
 Kemal.config.host_binding = ENV["HOST"]? || "0.0.0.0"
+
+Kemal.config.powered_by_header = false
